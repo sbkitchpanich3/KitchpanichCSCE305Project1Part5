4e1b08141927965956c992f3f67bfa55775c073f
sbkitchpanich3 <bankster007@gmail.com>
2017-10-17 11:49:23 -0800

    Initial commit

---
+++ app/src/main/java/com/sbkitchpanich/kitchpanichcsce305project1part5/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sbkitchpanich.kitchpanichcsce305project1part5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
